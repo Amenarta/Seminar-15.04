3fb51cb70dc0704659ae1bfe51858aa3f7fda2b1
Amenarta <karina1485@rambler.ru>
2022-04-17 23:17:20 +0300

    init commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача №1: Написать программу, которая выводит случайное число из отрезкаот 10 до 99 вкл.и показывает наибольшую цифру в этом числе.
+
+void shownumber ()
+{
+    int num = new Random().Next(10, 99);
+    int firstnum = num/10;
+    int secondnum = num%10;
+
+    Console.Write(num + "->");
+
+    if (firstnum > secondnum)
+    {
+        Console.WriteLine(firstnum);
+    }
+    else
+    {
+        Console.WriteLine(secondnum);
+    }
+}
+shownumber();
